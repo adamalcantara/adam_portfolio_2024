bfbd71690813b4a4f811441ab7d61baf1289b9fc
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-10 23:51:07 -0400

    Initial files added, hero added

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Hero } from "../components/Hero";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-500">
+      <Hero />
+    </div>
+  );
+}
