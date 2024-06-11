296a6703aca2725f4f297e835cc5df1df3d51ac3
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-11 00:09:15 -0400

    Vortex added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Hero } from "../components/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-slate-500">
+    <div className="">
       <Hero />
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,32 +1,25 @@
 "use client";
 
-import { motion } from "framer-motion";
 import React from "react";
-import { AuroraBackground } from "./ui/aurora-background";
+import { Vortex } from "./ui/vortex";
 
 export function Hero() {
   return (
-    <AuroraBackground>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
+    <div className="mx-auto h-[30rem] overflow-hidden">
+      <Vortex
+        backgroundColor="black"
+        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
       >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Background lights are cool you know.
+        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+          Greetings, I'm Adam
+        </h2>
+        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+          I'm a front end developer specializing in ReactJS and WordPress
+        </p>
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+          <button className="px-4 py-2 text-white border">View My Work</button>
         </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          And this, is chemical burn.
-        </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Debug now
-        </button>
-      </motion.div>
-    </AuroraBackground>
+      </Vortex>
+    </div>
   );
 }
