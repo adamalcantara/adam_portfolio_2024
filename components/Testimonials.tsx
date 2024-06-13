0be2e67c5c5586440a3cd022b57c122d4b1e6688
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-13 00:06:24 -0400

    Updated the testimonials

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -29,21 +29,4 @@ const testimonials = [
     name: "Patrick Moore",
     title: "Boot Camp Classmate",
   },
-  {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
-  },
-  {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
-  },
-  {
-    quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
-  },
 ];
