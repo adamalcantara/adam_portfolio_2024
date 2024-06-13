8cdee1f1b69e3ced931ba3574084a6bcadd40ea1
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-12 21:59:39 -0400

    Contact form added and styled

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,8 +5,10 @@ import ContactForm from './ui/ContactForm'
 
 const Contact = () => {
   return (
-    <div>
-        <ContactForm />
+    <div id='contact' className='flex justify-center p-10'>
+        <div className='w-1/2 sm:w-11/12 lg:w-9/12 p-10 bg-zinc-950 rounded-2xl border border-white/[0.2] '>
+          <ContactForm />
+        </div>
     </div>
   )
 }
--- components/ui/ContactForm.tsx
+++ components/ui/ContactForm.tsx
@@ -7,7 +7,8 @@ function ContactForm() {
       return <p>Thanks for joining!</p>;
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className='flex flex-col'>
+    <h1 className='text-2xl font-bold'>Contact Me</h1>
       <label htmlFor="email">
         Email Address
       </label>
@@ -15,23 +16,28 @@ function ContactForm() {
         id="email"
         type="email" 
         name="email"
+        className='rounded-md bg-zinc-700 border border-white/[0.3] focus:outline-1 focus:outline-white/[0.4]'
       />
       <ValidationError 
         prefix="Email" 
         field="email"
         errors={state.errors}
       />
+      <label htmlFor="message">
+        Message
+      </label>
       <textarea
         id="message"
         name="message"
+        className='rounded-md bg-zinc-700 border border-white/[0.3] focus:outline-1 focus:outline-white/[0.4]'
       />
       <ValidationError 
         prefix="Message" 
         field="message"
         errors={state.errors}
       />
-      <button type="submit" disabled={state.submitting}>
-        Submit
+      <button type="submit" disabled={state.submitting} className='w-full bg-zinc-800 p-2 mt-4 rounded-md border border-white/[0.3] hover:bg-zinc-600'>
+        Submit &rarr;
       </button>
     </form>
   );
